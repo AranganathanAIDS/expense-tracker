e28b2ded34602978010d6b36a972ff9e6cd5d27e
AranganathanAIDS <aranganathan.k2021ai@sece.ac.in>
2024-01-05 21:41:38 +0530

    add event handling

--- src/components/expenselist.js
+++ src/components/expenselist.js
@@ -1,75 +1,55 @@
+import ExpenseListItemComponent from "./ExpenseListItem";
+
 const ExpenseListComponent = () => {
-    return(
-        <>
-            <div class="layout-container__expenses">
+    const data = [
+        {
+            id: 1,
+            title: "Expense 1",
+            amount: 2000,
+            type: "Credit",
+            month: "January",
+            day: 2
+        },
+        {
+            id: 2,
+            title: "Expense 2",
+            amount: 500,
+            type: "Debit",
+            month: "January",
+            day: 4
+        },
+        {
+            id: 3,
+            title: "Expense 3",
+            amount: 20000,
+            type: "Credit",
+            month: "January",
+            day: 1
+        }
+    ];
+
+    return (
+        <div className="layout-container__wrapper">
+            <div className="flexbox flexbox-justify-between flexbox-align-baseline">
+                <h3>Expenses</h3>
+                <span className="pill info">INR 770</span>
+            </div>
+            <hr />
+            <div className="layout-container__expenses">
                 <ul>
-                    <li class="flexbox flexbox-justify-between">
-                        <div class="flexbox">
-                            <div class="flexbox flexbox-column flexbox-align-center date">
-                                <div class="month">January</div>
-                                <div class="day">1</div>
-                            </div>
-                            <div class="flexbox flexbox-align-center">
-                                <h3 class="title">Expense 2</h3>
-                                <span class="type-pill">Credit</span>
-                            </div>
-                        </div>
-                        <div class="flexbox flexbox-align-center">
-                            <div class="pill">2000 INR</div>
-                            <button class="actions">
-                                <span class="material-icons edit">edit</span>
-                            </button>
-                            <button class="actions">
-                                <span class="material-icons delete">delete</span>
-                            </button>
-                        </div>
-                    </li>
-                    <li class="flexbox flexbox-justify-between">
-                        <div class="flexbox">
-                            <div class="flexbox flexbox-column flexbox-align-center date">
-                                <div class="month">January</div>
-                                <div class="day">2</div>
-                            </div>
-                            <div class="flexbox flexbox-align-center">
-                                <h3 class="title">Expense 3</h3>
-                                <span class="type-pill">Debit</span>
-                            </div>
-                        </div>
-                        <div class="flexbox flexbox-align-center">
-                            <div class="pill">340 INR</div>
-                            <button class="actions">
-                                <span class="material-icons edit">edit</span>
-                            </button>
-                            <button class="actions">
-                                <span class="material-icons delete">delete</span>
-                            </button>
-                        </div>
-                    </li>
-                    <li class="flexbox flexbox-justify-between">
-                        <div class="flexbox">
-                            <div class="flexbox flexbox-column flexbox-align-center date">
-                                <div class="month">January</div>
-                                <div class="day">3</div>
-                            </div>
-                            <div class="flexbox flexbox-align-center">
-                                <h3 class="title">Expense 1</h3>
-                                <span class="type-pill">Debit</span>
-                            </div>
-                        </div>
-                        <div class="flexbox flexbox-align-center">
-                            <div class="pill">890 INR</div>
-                            <button class="actions">
-                                <span class="material-icons edit">edit</span>
-                            </button>
-                            <button class="actions">
-                                <span class="material-icons delete">delete</span>
-                            </button>
-                        </div>
-                    </li>
+                    <ExpenseListItemComponent
+                        data={data[0]}
+                    />
+                    <ExpenseListItemComponent
+                        data={data[1]}
+                    />
+                    <ExpenseListItemComponent
+                        data={data[2]}
+                    />
                 </ul>
             </div>
-        </>
-    );
-};
+        </div>
+    )
+}
 
 export default ExpenseListComponent;
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,35 +1,50 @@
-const SidebarComponent = () => {
-    return(
-        <>
-            <div class="sidebar-container">
-                <div class="sidebar-container__brand">
-                    Expense Tracker
-                </div>
-                <div class="sidebar-container__list">
-                    <ul>
-                        <li>
-                            <a class="active" href="/">
-                                <span class="material-icons">insights</span>
-                                <span>Expenses</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/add">
-                                <span class="material-icons">note_add</span>
-                                <span>Add Expense</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/analytics">
-                                <span class="material-icons">analytics</span>
-                                <span>Analytics</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+
+
+const SidebarComponent = ({ 
+    activeListItem, 
+    updateActiveListItem = () => {} 
+}) => {
+    // const [activeItem, setActiveItem] = useState(0);
+
+    // parent to child communication by calling the function
+    // passed by the parent component
+    const handleActiveItem = (e, index = 0) => {
+        // setActiveItem(index);
+        updateActiveListItem(index);
+    }
+    
+    return (
+        <div className="sidebar-container">
+            <div className="sidebar-container__brand">
+                Expense Tracker
             </div>
-        </>
-    );
-};
+            <div className="sidebar-container__list">
+                <ul>
+                    <li onClick={e => handleActiveItem(e, 0)}>
+                        {/* <a className={activeItem === 0 ? "active" : ""} href="javascript:void(0)"> */}
+                        <a className={activeListItem === 0 ? "active" : ""} >
+                            <span className="material-icons">insights</span>
+                            <span>Expenses</span>
+                        </a>
+                    </li>
+                    <li onClick={e => handleActiveItem(e, 1)}>
+                        {/* <a className={activeItem === 1 ? "active" : ""} href="javascript:void(0)"> */}
+                        <a className={activeListItem === 1 ? "active" : ""} >
+                            <span className="material-icons">note_add</span>
+                            <span>Add Expense</span>
+                        </a>
+                    </li>
+                    <li onClick={e => handleActiveItem(e, 2)}>
+                        {/* <a className={activeItem === 2 ? "active" : ""} href="javascript:void(0)"> */}
+                        <a className={activeListItem === 2 ? "active" : ""} >
+                            <span className="material-icons">analytics</span>
+                            <span>Analytics</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
 export default SidebarComponent;
